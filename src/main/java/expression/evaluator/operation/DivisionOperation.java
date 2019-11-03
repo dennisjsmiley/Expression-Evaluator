a5f710a7162279a4203cd5e88cc81152977700ba
Dennis Smiley <dennis.j.smiley@gmail.com>
2019-11-03 12:08:20 -0500

    Add DivisionOperation and test.

--- src/main/java/expression/evaluator/operation/DivisionOperation.java
+++ src/main/java/expression/evaluator/operation/DivisionOperation.java
@@ -1,4 +1,19 @@
 package expression.evaluator.operation;
 
-public class DivisionOperation {
+import expression.evaluator.node.Node;
+
+public class DivisionOperation extends BinaryOperation {
+    public DivisionOperation(Node left, Node right) {
+        super(left, right);
+    }
+
+    @Override
+    public double evaluate() {
+        return getLeft().evaluate() / getRight().evaluate();
+    }
+
+    @Override
+    public String toString() {
+        return getLeft().toString() + " / " + getRight().toString();
+    }
 }
--- src/test/java/expression/evaluator/operation/DivisionOperationTest.java
+++ src/test/java/expression/evaluator/operation/DivisionOperationTest.java
@@ -1,4 +1,27 @@
 package expression.evaluator.operation;
 
+import expression.evaluator.node.DoubleNumber;
+import expression.evaluator.node.Node;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.Assert.assertEquals;
+
 public class DivisionOperationTest {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Test
+    public void testDivisionOperation() {
+        Node eight = new DoubleNumber(8);
+        Node two = new DoubleNumber(2);
+        BinaryOperation divOp = new DivisionOperation(eight, two);
+
+        double result = divOp.evaluate();
+        assertEquals(4, result, 0);
+
+        logger.info(divOp.toString() + " = " + result);
+    }
+
 }
