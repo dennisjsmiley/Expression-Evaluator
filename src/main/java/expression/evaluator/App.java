27c21245a275f4beec3625b8539d5874b8b94a0b
Dennis Smiley <dennis.j.smiley@gmail.com>
2019-11-03 14:04:50 -0500

    Add main function with regex for breaking expression string into tokens.

---
+++ src/main/java/expression/evaluator/App.java
@@ -0,0 +1,29 @@
+package expression.evaluator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class App {
+
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+//        String input = "9 + 11 * 55 / .66 ^ 44.9 _ 2 * ( 2 + 1 )";
+        String input = "2 * 5 + 1";
+
+        String tokensRegex = "([+-]?([0-9]*[.])?[0-9]+)|(\\+)|(\\-)|(\\*)|(\\/)|(\\^)|(_)|(\\()|(\\))";
+
+        Pattern tokenPattern = Pattern.compile(tokensRegex);
+        Matcher matcher = tokenPattern.matcher(input);
+
+        while(matcher.find()) {
+            String token = matcher.group();
+            logger.info("token: {}", token);
+        }
+    }
+}
